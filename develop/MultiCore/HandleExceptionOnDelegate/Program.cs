6aa78ea316f6e81c07456ef441e5da3efca9d37f
yuichi <yuichi.ouchi@gmail.com>
2017-04-08 03:15:18 +0900

    Append Projects (Thread Exception Handlers)

---
+++ develop/MultiCore/HandleExceptionOnDelegate/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HandleExceptionOnDelegate
+{
+    class Program
+    {
+        delegate void DoWorkDelegate();
+        static bool isRunning = true;
+        static void Main(string[] args)
+        {
+
+            IAsyncResult ar = (new DoWorkDelegate(DoWork)).BeginInvoke(new AsyncCallback(WorkComplete), null);
+            Console.WriteLine("M: {0} BeginInvoke()",  DateTime.Now.ToString("ss.fff"));
+            ar.AsyncWaitHandle.WaitOne();
+            Console.WriteLine("M: {0} WaitOne()",  DateTime.Now.ToString("ss.fff"));
+
+            while (isRunning)
+                Thread.Sleep(100);
+
+            Console.WriteLine("Catch Exception by try-chach ...");
+            Console.ReadKey();
+
+        }
+
+        private static void WorkComplete(IAsyncResult ar)
+        {
+            DoWorkDelegate d = (DoWorkDelegate) ((AsyncResult)ar).AsyncDelegate;
+            try
+            {
+                d.EndInvoke(ar);
+            }
+            catch (Exception ex)
+            {
+                // EndInvolke()で例外をトラップできる
+                // ただし、ここはUIスレッドではないので注意!!
+                Console.WriteLine("Catch Exception at EndInvoke.");
+                Console.WriteLine("{0}: {1}", ex.GetType().Name, ex.Message);
+                Console.WriteLine("Hit any key to continue...");
+                Console.ReadKey();
+            }
+        }
+
+        private static void DoWork()
+        {
+            Console.WriteLine("T: {0} Thread Start",  DateTime.Now.ToString("ss.fff"));
+            Thread.Sleep(500);
+            Console.WriteLine("T: {0} Thread Raise Exception",  DateTime.Now.ToString("ss.fff"));
+            throw new InvalidOperationException("スレッドで発生した例外");
+        }
+    }
+}
---
+++ develop/MultiCore/HandleExceptionOnTaskWithContinue/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HandleExceptionOnTaskWithContinue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Task task = Task.Factory
+                            .StartNew(() => DoWork())
+                            .ContinueWith(t => DoContinue(t));
+            Console.WriteLine("M: {0} Task.Factory.StartNew()",  DateTime.Now.ToString("ss.fff"));
+            task.Wait();
+
+            Console.WriteLine("Hit any key to exit...");
+            Console.ReadKey();
+
+        }
+
+        private static void DoContinue(Task t)
+        {
+            Exception ex = t.Exception;
+            if(ex != null)
+            {
+                Console.WriteLine("C: Catch Exception by try-catch");
+                Console.WriteLine("Exception - {0}: {1}", ex.GetType().Name, ex.Message);
+                Console.WriteLine("InnerException - {0}: {1}", ex.InnerException.GetType().Name, ex.InnerException.Message);
+                Console.WriteLine("Hit any key to continue...");
+                Console.ReadKey();
+
+            }
+        }
+
+        private static void DoWork()
+        {
+            Console.WriteLine("T: {0} Thread Start", DateTime.Now.ToString("ss.fff"));
+            Thread.Sleep(500);
+            Console.WriteLine("T: {0} Thread Raise Exception", DateTime.Now.ToString("ss.fff"));
+            throw new InvalidOperationException("スレッドで発生した例外");
+        }
+    }
+}
